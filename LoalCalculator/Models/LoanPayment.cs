0fc58fb6b103aad389480591917083168c95482f
Dvlprdad <carlosmtamayo@gmail.com>
2021-09-21 21:24:08 -0400

    Updated LoanPayment Models

--- LoalCalculator/Models/LoanPayment.cs
+++ LoalCalculator/Models/LoanPayment.cs
@@ -7,5 +7,9 @@ namespace LoalCalculator.Models
 {
     public class LoanPayment
     {
+        public int Month { get; set; }
+        public decimal Payment { get; set; }
+        public decimal MonthlyPrincipal { get; set; }
+        public decimal MyProperty { get; set; }
     }
 }
